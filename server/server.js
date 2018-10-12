2bdf268bdf0e5b2e04140914182133ab2f002aef
syokujinau <npr286rx@rocketmail.com>
2018-10-12 13:08:44 +0800

    GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express'); //@4.14.0
 var bodyParser = require('body-parser'); //@1.15.2
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose'); 
 var {Todo} = require('./models/todo');
@@ -29,7 +30,28 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
-})
+});
+
+// GET /todos/1234
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    
+    // Valid id 
+    if (!ObjectID.isValid(id)){
+      return res.status(404).send('invalid id');
+    }
+
+    Todo.findById(id).then((todo) => {
+      if (!todo) {
+        return res.status(404).send('no data');
+      }
+
+      res.send({todo});
+    }).catch((e) => {
+      res.status(400).send();
+    })
+
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
