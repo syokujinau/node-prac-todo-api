8649db7a89a7667af314e1f433e1642629a65180
syokujinau <npr286rx@rocketmail.com>
2018-10-12 10:32:18 +0800

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text,
     completed: req.body.completed
+    //add body you want...
   });
 
   todo.save().then((doc) => {
@@ -21,6 +22,14 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
